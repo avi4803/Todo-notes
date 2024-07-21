494262ae6c34b16d72aabcde4e587bcac940aac7
Avinash Nishad <avinash.2023ug2013@iiitranchi.ac.in>
2024-07-21 21:36:28 +0530

    6

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       screens: {
-        'sm': '700px', // Custom value for 'sm'
+        'sm': '640px', // Custom value for 'sm'
         'md': '768px', // Custom value for 'md'
       },
 
